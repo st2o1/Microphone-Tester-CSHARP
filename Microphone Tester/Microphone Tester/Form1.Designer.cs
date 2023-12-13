a0237bd534e6d2a108a60b3b0dd9947fc5446af6
s21 <147557087+st2o1@users.noreply.github.com>
2023-12-13 20:07:11 +0100

    Add files via upload

---
+++ Microphone Tester/Microphone Tester/Form1.Designer.cs
@@ -0,0 +1,112 @@
+﻿namespace Microphone_Tester
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(130, 41);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Start";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(12, 72);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(369, 21);
+            this.comboBox1.TabIndex = 1;
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(148, 32);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(130, 21);
+            this.comboBox2.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 56);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(96, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Chose Microphone";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(148, 16);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(68, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Sample Rate";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(393, 102);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Microphone Tester";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Microphone Tester/Microphone Tester/Form1.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Collections.ObjectModel;   
+using NAudio.Wave;
+
+namespace Microphone_Tester
+{
+    public partial class Form1 : Form
+    {
+        private WaveIn waveIn;
+        private WaveOut waveOut;
+        private BufferedWaveProvider waveProvider;
+        private int selectedSampleRate;
+        public Form1()
+        {
+            InitializeComponent();
+            InitializeMicrophones();
+            InitializeSampleRates();
+        }
+
+        private void InitializeMicrophones()
+        {
+            var devices = new Collection<WaveInCapabilities>();
+
+            for (int i = 0; i < WaveIn.DeviceCount; i++)
+            {
+                devices.Add(WaveIn.GetCapabilities(i));
+            }
+            comboBox1.DataSource = devices;
+            comboBox1.DisplayMember = "ProductName";
+        }
+
+        private void InitializeSampleRates()
+        {
+            comboBox2.Items.AddRange(new object[] { 44100, 48000, 96000 });
+            comboBox2.SelectedIndex = 0;
+
+            selectedSampleRate = (int)comboBox2.SelectedItem;
+            comboBox2.SelectedIndexChanged += (sender, e) =>
+            {
+                selectedSampleRate = (int)comboBox2.SelectedItem;
+            };
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (waveIn == null)
+            {
+                StartListening();
+                button1.Text = "Stop";
+            }
+            else
+            {
+                StopListening();
+                button1.Text = "Start";
+            }
+        }
+        private void StartListening()
+        {
+            int selectedDeviceIndex = comboBox1.SelectedIndex;
+            if (selectedDeviceIndex == -1)
+            {
+                MessageBox.Show("Please select a microphone.");
+                return;
+            }
+            waveIn = new WaveIn();
+            waveIn.DeviceNumber = selectedDeviceIndex;
+            waveIn.DataAvailable += WaveInDataAvailable;
+            waveIn.WaveFormat = new WaveFormat(selectedSampleRate, 16, 1);
+            waveOut = new WaveOut();
+            waveProvider = new BufferedWaveProvider(new WaveFormat(selectedSampleRate, 16, 1));
+            waveOut.Init(waveProvider);
+            waveOut.Play();
+            waveIn.StartRecording();
+        }
+
+        private void WaveInDataAvailable(object sender, WaveInEventArgs e)
+        {
+            waveProvider.AddSamples(e.Buffer, 0, e.BytesRecorded);
+        }
+
+        private void StopListening()
+        {
+            if (waveIn != null)
+            {
+                waveIn.StopRecording();
+                waveIn.Dispose();
+                waveIn = null;
+            }
+
+            if (waveOut != null)
+            {
+                waveOut.Stop();
+                waveOut.Dispose();
+                waveOut = null;
+            }
+
+            if (waveProvider != null)
+            {
+                waveProvider.ClearBuffer();
+                waveProvider = null;
+            }
+        }
+    }
+}
